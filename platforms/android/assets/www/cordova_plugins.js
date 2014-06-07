2f4b52d20f199c5d25d7b26874b1741ac9133ec4
pereirazc <pereira.zc@gmail.com>
2014-06-07 00:14:12 -0300

    changing to ionic framework

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,55 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/com.red_folder.phonegap.plugin.backgroundservice/www/backgroundService.js",
+        "id": "com.red_folder.phonegap.plugin.backgroundservice.BackgroundService"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ionic.keyboard": "0.0.1",
+    "org.apache.cordova.console": "0.2.8",
+    "org.apache.cordova.device": "0.2.9",
+    "org.apache.cordova.inappbrowser": "0.4.1-dev",
+    "org.apache.cordova.network-information": "0.2.8",
+    "com.red_folder.phonegap.plugin.backgroundservice": "2.0.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ plugins/br.com.multicast.mobile/src/android/MulticastMobile.java
@@ -0,0 +1,67 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package br.com.multicast.mobile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.cordova.*;
+
+
+
+public class MulticastMobile extends CordovaActivity 
+{
+
+    //public static final String ACTION_CLOSE = "br.com.multicast.mobile.ACTION_CLOSE";
+    //private MulticastServiceReceiver receiver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.init();
+        String url;
+        if (this.getIntent().hasExtra("url")) {
+            url = this.getIntent().getStringExtra("url");
+        } else url = Config.getStartUrl();
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(url);
+        //super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    /*@Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+    class MulticastServiceReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_CLOSE)) {
+                //MulticastMobile.this.endActivity();
+            }
+        }
+    }*/
+
+}
+
